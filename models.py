4f05af9ce3d91d9dc1a123319af314056333110c
jennifer lyden <jlyden1@alumni.nd.edu>
2016-03-12 19:34:24 -0500

    Refactor: Further distinguish Game & Score classes, and associate User with each

--- models.py
+++ models.py
@@ -11,47 +11,53 @@ from datetime import date
 from protorpc import messages
 from google.appengine.ext import ndb
 
-
 # - - - - Objects - User - - - - -
 
 class User(ndb.Model):
-    """User profile"""
+    """ User profile """
     name = ndb.StringProperty(required=True)
     email = ndb.StringProperty(required=True)
+    total_games = ndb.IntegerProperty(default=0)
     wins = ndb.IntegerProperty(default=0)
-    games = ndb.IntegerProperty(default=0)
-    win_rate = ndb.FloatProperty(default=0.0)
-    total_penalty = ndb.IntegerProperty(default=0)
-    avg_penalty = ndb.FloatProperty(default=0.0)
-
 
     def user_to_form(self):
+    """ Populate UserForm """
         return UserForm(name=self.name,
                         email=self.email,
-                        games=self.games,
-                        win_rate=self.win_rate,
-                        avg_penalty=self.avg_penalty)
+                        total_games=self.total_games,
+                        win_rate=self.calc_win_rate())
+
+    def all_games(self):
+    """
+    Return all user games - in progress and complete
+    Reference: http://stackoverflow.com/questions/24392270/many-to-many-relationship-in-ndb
+    """
+        return Game.query().filter(Game.player_one == self.key \
+                                   or Game.player_two == self.key)
+
+    def all_scores(self):
+    """ Return all user scores - only from completed games """
+        return Score.query().filter(Score.winner == self.key \
+                                    or Score.loser == self.key)
+
+    def calc_win_rate(self):
+    """ Calculate win rate """
+        if total_games > 0:
+            win_rate = float(self.wins)/float(self.total_games)
+        else:
+            win_rate = 0.0
+        return win_rate
 
     def add_win(self):
-        """Add a win"""
+        """ Add a win """
         self.wins += 1
-        self.games += 1
-        self.win_rate = float(self.wins)/float(self.games)
+        self.total_games += 1
         self.put()
 
     def add_loss(self):
-        """Add a loss"""
-        self.games += 1
-        self.win_rate = float(self.wins)/float(self.games)
+        """ Add a loss """
+        self.total_games += 1
         self.put()
-
-    def add_penalty(self,penalty):
-        """Add to total_penalty"""
-        self.total_penalty += penalty
-        if self.total_penalty > 0:
-            self.avg_penalty = float(self.total_penalty)/float(self.games)
-        self.put()
-
 
 # - - - - Objects - Game - - - - -
 
@@ -67,9 +73,6 @@ class Game(ndb.Model):
     instructions = ndb.StringProperty()
     mid_move = ndb.BooleanProperty(required=True, default=False)
     game_over = ndb.BooleanProperty(required=True, default=False)
-    winner = ndb.KeyProperty()
-    penalty_one = ndb.IntegerProperty()
-    penalty_two = ndb.IntegerProperty()
     history = ndb.PickleProperty(required=True)
 
     @classmethod
@@ -106,8 +109,6 @@ class Game(ndb.Model):
                         draw_card=string_card,
                         mid_move=self.mid_move,
                         game_over=self.game_over)
-        if self.winner:
-            form.winner = self.winner.get().name
         return form
 
     def hand_to_form(self):
@@ -146,49 +147,46 @@ class Game(ndb.Model):
         chosen: boolean representing if active player has chosen to go "OUT"
         """
         # check both players' hands
-        self.penalty_one = test_hand(self.hand_one)
-        self.penalty_two = test_hand(self.hand_two)
+        penalty_one = test_hand(self.hand_one)
+        penalty_two = test_hand(self.hand_two)
 
         # if game ended because no more cards to draw
         if not chosen:
-            if self.penalty_one < self.penalty_two:
-                self.win_game(self.player_one)
-            elif self.penalty_two < self.penalty_one:
-                self.win_game(self.player_two)
+            if penalty_one < penalty_two:
+                score_game(self.player_one, penalty_one, penalty_two)
+            elif penalty_two < penalty_one:
+                score_game(self.player_two, penalty_two, penalty_one)
             # tie goes to active player
             else:
-                self.win_game(self.active)
+                score_game(self.active, penalty_one, penalty_two)
 
         # if game ended because active player signaled "OUT"
         # active player needs penalty of 0 to win
         else:
             if self.active == self.player_one:
                 if self.penalty_one == 0:
-                    self.win_game(self.player_one)
+                    score_game(self.player_one, penalty_one, penalty_two)
                 else:
-                    self.win_game(self.player_two)
+                    score_game(self.player_two, penalty_two, penalty_one)
             else:
                 if self.penalty_two == 0:
-                    self.win_game(self.player_two)
+                    score_game(self.player_two, penalty_two, penalty_one)
                 else:
-                    self.win_game(self.player_one)
-
-        # record player penalties
-        self.player_one.get().add_penalty(self.penalty_one)
-        self.player_two.get().add_penalty(self.penalty_two)
+                    score_game(self.player_one, penalty_one, penalty_two)
 
         self.mid_move = False
-        self.put()
-
-    def win_game(self, winner):
-        """Sets game winner"""
-        self.winner = winner
         self.game_over = True
         self.put()
 
+    def score_game(self, winner, penalty_winner, penalty_loser):
+        """Set up Score for Game"""
         loser = self.player_two if winner == self.player_one else self.player_one
         # Add the game to the score 'board'
-        score = Score(date=date.today(), winner=winner, loser=loser)
+        score = Score(date=date.today(),
+                      winner=winner,
+                      loser=loser,
+                      penalty_winner=penalty_winner,
+                      penalty_loser=penalty_loser)
         score.put()
 
         # Update the user models
@@ -197,7 +195,7 @@ class Game(ndb.Model):
 
     def history_to_form(self):
         """
-        Returns GameHistoryForm representation of completed Game
+        Returns GameHistoryForm representation of Game
         Assistance with list[tuples]->list[str]:
         http://stackoverflow.com/questions/11696078/python-converting-a-list-of-tuples-to-a-list-of-strings
         """
@@ -209,10 +207,6 @@ class Game(ndb.Model):
                                 player_two=self.player_two.get().name,
                                 game_over=self.game_over,
                                 history=history)
-        if self.winner:
-            form.winner = self.winner.get().name
-            form.penalty_one=self.penalty_one
-            form.penalty_two=self.penalty_two
         return form
 
 
@@ -221,13 +215,17 @@ class Game(ndb.Model):
 class Score(ndb.Model):
     """Score object"""
     date = ndb.DateProperty(required=True)
-    winner = ndb.KeyProperty(required=True)
-    loser = ndb.KeyProperty(required=True)
+    winner = ndb.KeyProperty(required=True, kind="User")
+    loser = ndb.KeyProperty(required=True, kind="User")
+    penalty_winner = ndb.IntegerProperty(required=True)
+    penalty_loser = ndb.IntegerProperty(required=True)
 
     def score_to_form(self):
         return ScoreForm(date=str(self.date),
                          winner=self.winner.get().name,
-                         loser=self.loser.get().name)
+                         loser=self.loser.get().name,
+                         penalty_winner=self.penalty_winner,
+                         penalty_loser=self.penalty_loser)
 
 
 # - - - - Forms - User - - - - -
@@ -236,9 +234,8 @@ class UserForm(messages.Message):
     """User Form"""
     name = messages.StringField(1, required=True)
     email = messages.StringField(2)
-    games = messages.IntegerField(3, required=True)
+    total_games = messages.IntegerField(3, required=True)
     win_rate = messages.FloatField(4)
-    avg_penalty = messages.FloatField(5)
 
 class UserForms(messages.Message):
     """Container for multiple User Forms"""
@@ -261,7 +258,6 @@ class GameForm(messages.Message):
     draw_card = messages.StringField(5, required=True)
     mid_move = messages.BooleanField(6, required=True)
     game_over = messages.BooleanField(7, required=True)
-    winner = messages.StringField(8)
 
 class GameForms(messages.Message):
     """Container for multiple GameForm"""
@@ -282,10 +278,7 @@ class GameHistoryForm(messages.Message):
     player_one = messages.StringField(2, required=True)
     player_two = messages.StringField(3, required=True)
     game_over = messages.BooleanField(4, required=True)
-    winner = messages.StringField(5)
-    penalty_one = messages.IntegerField(6)
-    penalty_two = messages.IntegerField(7)
-    history = messages.StringField(8)
+    history = messages.StringField(5)
 
 class MoveForm(messages.Message):
     """Used to make a move in an existing game"""
@@ -300,6 +293,8 @@ class ScoreForm(messages.Message):
     date = messages.StringField(1, required=True)
     winner = messages.StringField(2, required=True)
     loser = messages.StringField(3, required=True)
+    penalty_winner = messages.IntegerField(4, required=True)
+    penalty_loser = messages.IntegerField(5, required=True)
 
 class ScoreForms(messages.Message):
     """Return multiple ScoreForms"""
